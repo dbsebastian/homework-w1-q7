87fc23459c6005d2c8315a6f09118da7c5a05945
Sebastian B <45515796+dbsebastian@users.noreply.github.com>
2024-01-25 03:43:40 +0000

    043hs

--- variables.tf
+++ variables.tf
@@ -1,11 +1,19 @@
 variable "GUGL_NAME_PROYECT" {
+  description = "Nombre del projecto"
+  default     = "taxi-de-pr"
 }
 
 variable "GOOGLE_STORAGE_BUCKET_NAME" {
+  description = "My Storage Bucket Name"
+  default     = "taxi-de-pr-demo-storage"
 }
 
 variable "BIGQ_DATASET_NAME" {
+  description = "My Bigquery Dataset Name"
+  default     = "demo_dataset_name"
 }
 
 variable "GOOGLE_CREDENTIALS" {
+    description =   "permiso-json de la cuenta"
+    default     =   "mipermiso_covid.json"
 }
